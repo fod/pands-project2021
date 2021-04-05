4ecdb08430fcb7fea7a93dcd24bf5e5f21d11c22
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-04-05 23:02:39 +0100

    analysis.py library imports

--- analysis.py
+++ analysis.py
@@ -2,5 +2,11 @@
 # Perform various analyses on Fisher's Iris dataset
 # Author: Fiachra O' Donoghue
 
+# Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pyplot
+import seaborn as sns
 
-# Summary stats on each variable
+
+
