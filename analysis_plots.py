cfedba86e1bc0e5385dc6d1c3d20abb4891a9aaa
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-04-24 12:42:54 +0100

    added analysis-plots.py with some function stubs

---
+++ analysis_plots.py
@@ -0,0 +1,25 @@
+# analysis_plots.py
+# Plot output functions for analysis of Fisher's iris data set
+# Author: Fiachra O' Donoghue
+
+"""
+These functions abstract away some of the details of producing 
+plots from the iris dataset while also providing a simple interface
+for some basic tinkering with the plots. 
+
+Keeping these functions in a dedicated plots file will hopefully keep
+the main analysis file more manageable in terms of length and comlexity.
+"""
+
+
+def histograms():
+    pass
+
+def ecdfs():
+    pass
+
+def stripplots():
+    pass
+
+def catplots():
+    pass
